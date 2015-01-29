47fd4afe5ea9040ff928340696caa2622c08d9a2
KarnageKnight <karnageknight@KarnageKnights-MacBook-Pro.local>
2015-01-29 15:02:50 +0530

    Initial Commit

---
+++ Queue_push_pop/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Queue_push_pop
+//
+//  Created by KarnageKnight on 29/01/15.
+//  Copyright (c) 2015 KKCPP. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
